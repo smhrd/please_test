5ae5694b6ecb3219c74b7484a8820ee1fcf14f13
smartIT8150 <mjl7173@naver.com>
2017-07-13 21:46:02 +0900

    수정수정~

--- simpleJavaTest/src/simpleJavaTest/SimpleMain.java
+++ simpleJavaTest/src/simpleJavaTest/SimpleMain.java
@@ -5,7 +5,27 @@ public class SimpleMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("æ»≥Á2222222");
-		
+		System.out.println("«œ¿Ã");
+
+		int cnt = 0;
+		for (int i = 1; i <= 50; i++) {
+
+			if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+				cnt++;
+			if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
+				cnt++;
+			}
+
+			if (cnt == 1) {
+				System.out.print("¬¶! ");
+			} else if (cnt == 2) {
+				System.out.print("¬¶¬¶! ");
+			} else {
+				System.out.print(i + " ");
+			}
+			cnt=0;
+
+		}
 	}
 
 }
