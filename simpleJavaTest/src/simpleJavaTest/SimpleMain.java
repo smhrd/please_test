24a44ed6c0d3e97af61065558b0ed941a62d2601
smartIT8150 <mjl7173@naver.com>
2017-07-13 21:35:23 +0900

    kkkkk

--- simpleJavaTest/src/simpleJavaTest/SimpleMain.java
+++ simpleJavaTest/src/simpleJavaTest/SimpleMain.java
@@ -4,7 +4,8 @@ public class SimpleMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("¾È³ç");
+		System.out.println("¾È³ç2222222");
+		
 	}
 
 }
